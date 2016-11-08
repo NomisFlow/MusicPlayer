36a0cc79672d1f48a17c42ec486e4df6a117ae96
Simon Wolf <simon.wolf1999@gmail.com>
2016-11-08 22:48:39 +0100

    Started implementing the Percentage counter

--- GUI.java
+++ GUI.java
@@ -8,10 +8,11 @@ public class GUI extends JFrame
     JButton play;
     JButton pause;
     JButton close;
-
+    JLabel percentage;
+    
     JTextField browser;
     JButton open;
-
+    
     public static void main(String[] args){
         GUI g = new GUI();
     }
@@ -30,20 +31,30 @@ public class GUI extends JFrame
         play = new JButton("Play");
         pause = new JButton("Pause");
         close = new JButton("Close");
+        percentage = new JLabel("Fortschritt: __%");
 
-        browser = new JTextField("                                           ");
+        browser = new JTextField("song.mp3");
         open = new JButton("open");
 
         close.addActionListener(e -> beenden());
         open.addActionListener(e -> open());
-        browser.addMouseListener(e -> browser.setText(""));
+        browser.addMouseListener((ClickedListener)(e -> browser.setText("")));
+        pause.addActionListener(e -> m.pause());
+        play.addActionListener(e -> play());
+        
 
         add(play);
         add(pause);
+        add(percentage);
         add(browser);
         add(open);
         add(close);
-
+        
+        while(m.getPositionInPercent() <= 99){
+            percentage.setText("Fortschritt:" + m.getPositionInPercent() + " %");
+            
+        }
+        
         pack();
         setSize(450, 80);
     }
@@ -58,6 +69,14 @@ public class GUI extends JFrame
         m.open(browser.getText());
         m.play();
     }
+    
+    private void play(){
+        if(m.getPositionInPercent() == 0)
+            m.play();
+        else if(m.getPositionInPercent() > 0)
+            m.resume();
+    }
+    
 
     
     
